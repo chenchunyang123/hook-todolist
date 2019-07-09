6e93c7f004489e1e71722bbcc6f68bc878a59983
chenchunyang12345 <chenchunyang@hual.ai>
2019-07-09 14:37:11 +0800

    update

---
+++ build/precache-manifest.95d99fa7deb370178bc26474b83eb1f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca6ec89fabfd8160c4982464f09574b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caf325240ba71082c389",
+    "url": "./static/css/main.1200cf81.chunk.css"
+  },
+  {
+    "revision": "af1b6b08e2a705d0b8e7",
+    "url": "./static/js/2.7b0e938e.chunk.js"
+  },
+  {
+    "revision": "caf325240ba71082c389",
+    "url": "./static/js/main.beb662dc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
